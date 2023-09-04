de67fa96722e13bc1b810a591db16c9bed47dd37
Schlumen <lumen.haendler@web.de>
2023-09-04 11:14:02 +0200

    Implement User model tests

--- A2_Recipe_App/src/users/models.py
+++ A2_Recipe_App/src/users/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class User(models.Model):
     name = models.CharField(max_length=120)
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=120)
     bio = models.TextField(null=True)
     email = models.EmailField(max_length=120)
 
