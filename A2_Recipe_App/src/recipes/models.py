c5336e2e22485678f18e36a22e136ec835be8825
Schlumen <lumen.haendler@web.de>
2023-09-04 09:56:04 +0200

    Define, register and migrate models

--- A2_Recipe_App/src/recipes/models.py
+++ A2_Recipe_App/src/recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=120)
+    ingredients = models.CharField(max_length=512)
+    cooking_time = models.IntegerField()
+    difficulty = models.CharField(max_length=20)
+    description = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Recipe: {self.name}"
--- A2_Recipe_App/src/users/models.py
+++ A2_Recipe_App/src/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=120)
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(null=True)
+    email = models.EmailField(max_length=120)
+
+    def __str__(self):
+        return f"User: {self.username} ({self.name} - {self.email})"
